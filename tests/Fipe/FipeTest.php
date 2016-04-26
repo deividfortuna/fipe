f7399d3a0bbda093746838185d15bd1f866948ed
Alison <alisonmonteiro.10@gmail.com>
2016-04-25 23:46:44 -0300

    Add some tests

--- tests/Fipe/FipeTest.php
+++ tests/Fipe/FipeTest.php
@@ -2,12 +2,50 @@
 
 namespace Fipe\Tests;
 
+use Fipe\Fipe;
 use PHPUnit_Framework_TestCase as PHPUnit;
 
 class FipeTest extends PHPUnit
 {
-    public function test()
+    public function testTypeHandler()
     {
+        $fipe = new Fipe('carros');
+        $this->assertEquals('carros', $fipe->getTipo());
+        $fipe->setTipo('motos');
+        $this->assertEquals('motos', $fipe->getTipo());
+    }
 
+    public function testGetBrands()
+    {
+        $fipe = new Fipe('motos');
+        $brands = $fipe->getMarcas();
+
+        $this->assertInternalType('array', $brands);
+        $this->assertNotEquals(0, count($brands));
+    }
+
+    public function testGetModels()
+    {
+        $fipe = new Fipe('motos');
+        $models = $fipe->getModelos(80);
+
+        $this->assertInternalType('object', $models);
+    }
+
+    public function testGetYears()
+    {
+        $fipe = new Fipe('motos');
+        $years = $fipe->getAnos(80, 3841);
+
+        $this->assertInternalType('array', $years);
+        $this->assertNotEquals(0, count($years));
+    }
+
+    public function testGetVehicle()
+    {
+        $fipe = new Fipe('motos');
+        $vehice = $fipe->getVeiculo(80, 3841, '2015-1');
+
+        $this->assertInternalType('object', $vehice);
     }
 }
